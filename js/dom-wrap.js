326d93041cf7e3025ffd8fd80fc824e4f28ebb0e
youjingyu <735284268@qq.com>
2017-10-26 15:09:37 +0800

    fix event agent logic

--- js/dom-wrap.js
+++ js/dom-wrap.js
@@ -5,32 +5,55 @@
         if(/^#/.test(css_selector)){
             dom = document.querySelector(css_selector);
             dom.getEle = Cr.getEle;
-            dom.addEvent = addEvent;
+            dom.on = addEvent;
         } else {
             dom = document.querySelectorAll(css_selector);
             // add getEle and addEvent method for each elemrnt to chain call
             [].forEach.call(dom, function (ele) {
                 ele.getEle = Cr.getEle;
-                ele.addEvent = addEvent;
+                ele.on = addEvent;
             });
         }
         return dom;
     }
     function addEvent(type, target_sel, callback) {
+        var _this = this;
         this.addEventListener(type, function (event) {
             if(typeof target_sel === 'function'){
-                target_sel();
+                target_sel.call(_this, event);
             } else if(typeof target_sel === 'string'){
-                // event agent, only surpport tagName, id, and class selector
-                target_sel = target_sel.replace(/\.|#/, '');
                 var target = event.target;
-                // if target match the selector
-                if(target.tagName.toLowerCase() === target_sel ||
-                    target.getAttribute('id') === target_sel ||
-                    new RegExp(target_sel).test(target.className)){
-                    callback && callback();
+                var test = testFunc(target_sel);
+                while(target){
+                    // if target match the selector
+                    if(test(target)){
+                        callback && callback.call(target, event);
+                    }
+                    target = target.parentNode;
+                    // traverse to the agent element
+                    if(target.isEqualNode(this)){
+                        break;
+                    }
                 }
             }
-        })
+            // the selector of event agent, only surpport tagName, id, and class selector
+            function testFunc(selctor) {
+                var sel;
+                if(/^#/.test(selctor)){
+                    // id selector
+                    sel = selctor.replace('#', '')
+                    return function (target) { return target.getAttribute('id') === sel }
+                } else if(/^\./.test(selctor)){
+                    // class selector
+                    sel = selctor.replace('.', '');
+                    var reg = new RegExp(sel)
+                    return function (target) { return reg.test(target.className) }
+                } else {
+                    // tagname selector
+                    return function (target) { return target.tagName.toLowerCase() === selctor }
+                }
+            }
+        });
+
     }
 })(window.Cr || (window.Cr = {}));
