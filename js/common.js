1b358bcdc2cf20952145aea31bd95023527e0887
youjingyu <735284268@qq.com>
2017-10-26 15:09:56 +0800

    add event to show dialog

--- js/common.js
+++ js/common.js
@@ -1,1 +1,15 @@
-Cr.render('agent_list', Cr.getData().agents.physical)
+(function () {
+    // config js hook
+    var js_hook = {
+        agent_list_templ: 'agent_list_templ',
+        agent_list: '#agent_list',
+        add_res: '.js-add-res'
+    }
+    // init agents list
+    Cr.render(js_hook.agent_list_templ, Cr.getData().agents.physical);
+    // add event listener
+    var $agent_list = Cr.getEle(js_hook.agent_list);
+    $agent_list.on('click', js_hook.add_res, function () {
+        this.parentNode.nextSibling.nextSibling.setAttribute('style', 'display: block');
+    });
+})()
