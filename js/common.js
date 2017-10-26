ba49ae2b8653c97d4ede52ce6f17aa0ac7f5bc73
whale <735284268@qq.com>
2017-10-26 22:43:05 +0800

    refactor the initialization process

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-(function () {
+(function (Cr) {
     // config js hook
     var js_hook = {
         agent_list_templ: 'agent_list_templ',
@@ -9,27 +9,32 @@
         dialog_close: '.js-dialog-close',
         dialog_add: '.js-dialog-add'
     }
-    // init agents list
-    Cr.render(js_hook.agent_list_templ, Cr.getData().agents.physical);
 
     var $agent_list = Cr.getEle(js_hook.agent_list);
     // delete resources
     $agent_list.on('click', js_hook.delete_res, function () {
         var parrent = this.parentNode;
+        Cr.deleteRes('');
         parrent.parentNode.removeChild(parrent);
     });
-
-    var dialogs = Cr.getEle(js_hook.dialog);
     // show dialog
     $agent_list.on('click', js_hook.add_res, function () {
         this.parentNode.nextSibling.nextSibling.setAttribute('style', 'display: block');
     });
-    // hide dialog
-    dialogs.on('click', js_hook.dialog_close, function () {
-        this.parentNode.parentNode.setAttribute('style', 'display: none');
-    });
-    // dialog add
-    dialogs.on('click', js_hook.dialog_add, function () {
-        this.parentNode.parentNode.setAttribute('style', 'display: none');
-    });
-})()
+
+    var dialogs = Cr.getEle(js_hook.dialog);
+    Cr.refreshList = function (agent_type) {
+        var data = Cr.getData();
+        var list_data = agent_type === 'all' ? data['physical'].concat(data['virtual']) : data[agent_type]
+        // init agents list
+        Cr.render(js_hook.agent_list_templ, list_data);
+        // hide dialog
+        dialogs.on('click', js_hook.dialog_close, function () {
+            this.parentNode.parentNode.setAttribute('style', 'display: none');
+        });
+        // dialog add
+        dialogs.on('click', js_hook.dialog_add, function () {
+            this.parentNode.parentNode.setAttribute('style', 'display: none');
+        });
+    };
+})(window.Cr || (window.Cr = {}))
--- js/data.js
+++ js/data.js
@@ -1,25 +1,30 @@
 (function(Cr){
-    Cr.getData = function () {
-        return {
-            agents: {
-                physical: [
-                    {
-                        name: 'bjstdmngbgr02.thoughtworks.com',
-                        status: 'idle',
-                        ip: '192.168.1.2',
-                        path: '/var/lib/cruise-agent',
-                        resources: ['ubuntu', 'firefox3', 'core-duo']
-                    },
-                    {
-                        name: 'bjstdmngbgr02.thoughtworks.com',
-                        status: 'building',
-                        ip: '192.168.1.2',
-                        path: '/var/lib/cruise-agent',
-                        resources: ['ubuntu', 'firefox3', 'core-duo']
-                    }
-                ],
-                virtual: [],
+    var data = {
+        physical: [
+            {
+                name: 'bjstdmngbgr02.thoughtworks.com',
+                status: 'idle',
+                ip: '192.168.1.2',
+                path: '/var/lib/cruise-agent',
+                resources: ['ubuntu', 'firefox3', 'core-duo']
+            },
+            {
+                name: 'bjstdmngbgr02.thoughtworks.com',
+                status: 'building',
+                ip: '192.168.1.2',
+                path: '/var/lib/cruise-agent',
+                resources: ['ubuntu', 'firefox3', 'core-duo']
             }
-        }
+        ],
+        virtual: [],
+    };
+    Cr.getData = function () {
+        return data;
+    };
+    Cr.addRes = function (type, resource) {
+        data[type].resources.push(resource);
+    }
+    Cr.deleteRes = function (type, index) {
+        data[type].resources.splice(index, 1);
     }
 })(window.Cr || (window.Cr = {}));
